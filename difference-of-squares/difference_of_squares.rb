64ca58a21a0125ecab95065d7d1c704cbab5d86d
branquinhoaa <amanda.alves15@gmail..com>
2017-02-27 14:26:09 -0300

    fix rubocop offenses

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -1,15 +1,15 @@
-# Find the difference between the sum of the squares and the square of the sum of the first N natural numbers.
+# Find the difference between the sum of the squares and the square
+# of the sum of the first N natural numbers.
 
 # The square of the sum of the first ten natural numbers is
-# (1 + 2 + ... + 10)² = 55² = 3025.
+# (1 + 2 + ... + 10)^2 = 55^2 = 3025.
 
 # The sum of the squares of the first ten natural numbers is
-# 1² + 2² + ... + 10² = 385.
+# 1^2 + 2^2 + ... + 10^2 = 385.
 
 # Hence the difference between the square of the sum of the first
 # ten natural numbers and the sum of the squares of the first ten
 # natural numbers is 3025 - 385 = 2640.
-
 class Squares
   def initialize(num)
     @qty = num
