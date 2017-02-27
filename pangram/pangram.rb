43a74a111a1f8cb997c14933a7909be6df162ac9
branquinhoaa <amanda.alves15@gmail..com>
2017-02-27 15:34:58 -0300

    rubocop fixes

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -5,9 +5,9 @@
 # The best known English pangram is "The quick brown fox jumps over the lazy dog."
 # The alphabet used is ASCII, and case insensitive, from 'a' to 'z'
 # inclusively.
- class Pangram
-   def self.pangram?(sentence)
-     return false unless ((97..122).to_a - sentence.downcase.codepoints).empty?
-     true
-   end
- end
+class Pangram
+  def self.pangram?(sentence)
+    return false unless ((97..122).to_a - sentence.downcase.codepoints).empty?
+    true
+  end
+end
