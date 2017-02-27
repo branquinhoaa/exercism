ac34b56d23126be30ba2297216cec4031f158fdf
branquinhoaa <amanda.alves15@gmail..com>
2017-02-27 15:52:01 -0300

    include alphabet constant and version

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,3 +1,7 @@
+module BookKeeping
+  VERSION = 4 # Where the version number matches the one in the test.
+end
+
 # Pangram
 # Determine if a sentence is a pangram.
 # Determine if a sentence is a pangram. A pangram (Greek: παν γράμμα, pan gramma,
@@ -6,8 +10,9 @@
 # The alphabet used is ASCII, and case insensitive, from 'a' to 'z'
 # inclusively.
 class Pangram
+  ALPHABET_RANGE = (97..122).to_a
+
   def self.pangram?(sentence)
-    return false unless ((97..122).to_a - sentence.downcase.codepoints).empty?
-    true
+    (ALPHABET_RANGE - sentence.downcase.codepoints).empty?
   end
 end
