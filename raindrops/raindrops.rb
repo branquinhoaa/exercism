1129ba4af8c92f44ac8325ef9e3dab2cf7288a97
branquinhoaa <amanda.alves15@gmail..com>
2017-02-27 14:29:12 -0300

    fix rubocop offenses

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,5 +1,7 @@
 ## Raindrops
-#Convert a number to a string, the contents of which depend on the number's factors.
+# Convert a number to a string, the contents of which depends
+# on the number's factors.
+#
 #- If the number has 3 as a factor, output 'Pling'.
 #- If the number has 5 as a factor, output 'Plang'.
 #- If the number has 7 as a factor, output 'Plong'.
@@ -12,7 +14,6 @@
 #  - In raindrop-speak, this would be a "PlingPlang".
 #- 34 has four factors: 1, 2, 17, and 34.
 #  - In raindrop-speak, this would be "34".
-
 class Raindrops
   def self.convert(num)
     answer = ''
